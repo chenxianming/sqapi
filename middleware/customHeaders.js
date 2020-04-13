03c5e39657abefe3a4ca583bf13a5044344fe0e7
chenxianming <hello@c-xm.com>
2020-04-13 21:56:09 +0800

    Update customHeaders.js

--- middleware/customHeaders.js
+++ middleware/customHeaders.js
@@ -2,7 +2,10 @@ const setCustomHeaders = (req, res, next) => {
 
     //res.setHeader('Content-Security-Policy','upgrade-insecure-requests');
     res.setHeader('X-Server-Frame', 'sqapi');
-
+    res.setHeader('Access-Control-Allow-Origin', '*'); // customize your FE host
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.setHeader("Access-Control-Allow-Headers","protobuf, token, key, Content-Type"); // for get method, pass token, key and Content-Type
+    
     next();
 }
 
